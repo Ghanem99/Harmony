f3731815e8b150d6e84bccf40ddce06499048fa6
Ghanem99 <moghanem345@gmail.com>
2024-02-08 21:36:00 +0200

    add the relation between User-Note models

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class NoteController extends Controller
 {
-    //
+    public function index() {
+        $notes = Note::all();
+
+        
+    }
 }
--- database/migrations/2024_01_29_124434_create_users_table.php
+++ database/migrations/2024_01_29_124434_create_users_table.php
@@ -8,7 +8,7 @@ class CreateUsersTable extends Migration {
 	public function up()
 	{
 		Schema::create('users', function(Blueprint $table) {
-			$table->increments('id');
+			$table->id('id');
 			$table->string('name');
 			$table->string('email');            
 			$table->timestamp('email_verified_at')->nullable();
