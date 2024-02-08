93c6aee98887941e62986d73d58e71a0375abb39
Ghanem99 <moghanem345@gmail.com>
2024-02-08 21:09:42 +0200

    build the Note migration file

--- database/migrations/2024_02_05_153457_create_habits_table.php
+++ database/migrations/2024_02_05_153457_create_habits_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('habits', function(Blueprint $table) {
             $table->id();
-			$table->integer('user_id')->constrained('users')
+			$table->foreignId('user_id')->constrained('users')
 				->cascadeOnDelete();
 			$table->string('title');
 			$table->string('color');
