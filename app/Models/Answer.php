ff0d661298196b9601d5f2a14c26c7a83ae202f6
Ghanem99 <moghanem345@gmail.com>
2024-03-09 17:03:37 +0200

    create Answer model and define the question inside!

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Answer extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+      'answer', 'score'
+    ];
+
+    public function question(): BelongsTo
+    {
+        return $this->belongsTo(Question::class);
+    }
 }
