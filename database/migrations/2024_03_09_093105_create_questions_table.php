3056402c70c2807c5aefa85caec693fcc80240c3
Ghanem99 <moghanem345@gmail.com>
2024-04-24 23:38:32 +0200

    add Columns name for question table!

--- database/migrations/2024_03_09_093105_create_questions_table.php
+++ database/migrations/2024_03_09_093105_create_questions_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->default('question');
             $table->foreignId('survey_id')->constrained('surveys')
                 ->cascadeOnDelete();
             $table->timestamps();
