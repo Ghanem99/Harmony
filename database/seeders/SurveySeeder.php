003b6c310aadff61590840d7aa28219cba77f508
Ghanem99 <moghanem345@gmail.com>
2024-04-24 23:50:35 +0200

    Create Survey Seeder Test!

---
+++ database/seeders/SurveySeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Survey;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class SurveySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Survey::create([
+            'name' => 'Diagnose'
+        ]);
+    }
+}
