659e83099b12f4eb90df46e95dc19528f4fb9433
Ghanem99 <moghanem345@gmail.com>
2024-05-03 22:12:09 +0300

    Group the article Under Content System!

--- app/Filament/Resources/ArticleResource.php
+++ app/Filament/Resources/ArticleResource.php
@@ -19,7 +19,8 @@ class ArticleResource extends Resource
 {
     protected static ?string $model = Article::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-map';
+    protected static ?string $navigationGroup = 'Content Management';
 
     public static function form(Form $form): Form
     {
