e5b8e6ff258c8afc2bd01f3713a8efb1b70fcc22
Ghanem99 <moghanem345@gmail.com>
2024-05-04 19:49:44 +0300

    Create Podcast Resource CRUD!

---
+++ app/Filament/Resources/PodcastResource.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PodcastResource\Pages;
+use App\Filament\Resources\PodcastResource\RelationManagers;
+use App\Models\Podcast;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PodcastResource extends Resource
+{
+    protected static ?string $model = Podcast::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-microphone';
+    protected static ?string $navigationGroup = 'Content Management';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')
+                    ->label('Title')
+                    ->required(),
+                Textarea::make('description')
+                    ->label('Description')
+                    ->required(),
+                FileUpload::make('file')
+                    ->label('File')
+                    ->required(),
+                FileUpload::make('image')
+                    ->label('Image')
+                    ->required(),
+                Select::make('category_id')
+                    ->label('Category')
+                    ->relationship('category', 'name')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('description')
+                    ->searchable()
+                    ->sortable(),
+                ImageColumn::make('image')
+                    ->label('Image'),
+                TextColumn::make('category.name')
+                    ->label('Category')
+                    ->searchable()
+                    ->sortable(),
+            ])
+            ->filters([
+                // filter by category
+                Tables\Filters\SelectFilter::make('category_id')
+                    ->label('Category')
+                    ->relationship('category', 'name'),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPodcasts::route('/'),
+            'create' => Pages\CreatePodcast::route('/create'),
+            'edit' => Pages\EditPodcast::route('/{record}/edit'),
+        ];
+    }
+}
