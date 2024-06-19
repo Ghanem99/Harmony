e186dc502977c00ce835fcc7b6e64fa71f4b6bf2
Ghanem99 <moghanem345@gmail.com>
2024-06-19 21:54:50 +0300

    Install Spatie Permissions + Create Roles and Permission along with Users!

--- app/Filament/Resources/PodcastResource.php
+++ app/Filament/Resources/PodcastResource.php
@@ -74,6 +74,7 @@ class PodcastResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
