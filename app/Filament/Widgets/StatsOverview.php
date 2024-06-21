b58f46e252abc720c141c65f777debaa27004d6d
Ghanem99 <moghanem345@gmail.com>
2024-06-22 00:07:43 +0300

    Update stats!

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,6 +2,9 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Plan;
+use App\Models\User;
+use App\Models\Breath;
 use App\Models\Survey;
 use App\Models\Article;
 use App\Models\Podcast;
@@ -15,15 +18,27 @@ class StatsOverview extends BaseWidget
         return [
             Stat::make('Articles', Article::count())
                 ->color('success')
-                ->description('All articles in the system')
-                ->chart([7, 6, 5, 4, 3, 2, 1]), // TODO: replace with actual date,
+                ->description('All Articles in the system')
+                ->chart([7, 6, 5, 4, 3, 2, 1]), 
             Stat::make('Podcasts', Podcast::count())
                 ->color('primary')
-                ->description('All podcasts in the system')
+                ->description('All Podcasts in the system')
                 ->chart([1, 2, 3, 4, 5, 6, 7]),
             Stat::make('Surveys', Survey::count())
                 ->color('warning')
                 ->description('All surveys in the system')
+                ->chart([3, 2, 1, 2, 3, 4, 5]),
+            Stat::make('Users', User::count())
+                ->color('warning')
+                ->description('All Users in the system')
+                ->chart([3, 2, 1, 2, 3, 4, 5]),
+            Stat::make('Breaths', Breath::count())
+                ->color('warning')
+                ->description('All Breaths in the system')
+                ->chart([3, 2, 1, 2, 3, 4, 5]),
+            Stat::make('Plans', Plan::count())
+                ->color('warning')
+                ->description('All Generated Plans in the system')
                 ->chart([3, 2, 1, 2, 3, 4, 5]),
         ];
     }
